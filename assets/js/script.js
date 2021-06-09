802732b5d5de4893b1f378271344a505c10b4dd9
KSanthanesh <santhanesh_2004@yahoo.com>
2021-06-09 21:32:31 +0000

    Add code for memory game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,68 +1,48 @@
-document.addEventListener('DOMContentLoaded', () => {
+// Array of deck of card images
+const deckCards = ["black-rose.jpg", "black-rose.jpg", "green-rose.jpg", "green-rose.jpg", "pink-rose.jpg", "pink-rose.jpg", "red-rose.png", "red-rose.png", "white-rose.jpg", "white-rose.jpg", "yellow-rose.jpg", "yellow-rose.jpg",];
+// global Arrays
+// access the <ul> with the class of .deck
+let deck = document.querySelector(".deck");
 
-    //card options
-    let cardArray = [
-        {
-        name: 'white-rose',
-            img: 'assets/images/white-rose.jpg',
-        }, {
-            name: 'white-rose',
-            img: 'assets/images/white-rose.jpg',
-        }, {
-            name: 'black-rose',
-            img: 'assets/images/black-rose.jpg',
-        }, {
-            name: 'black-rose',
-            img: 'assets/images/black-rose.jpg',
-        }, {
-            name: 'green-rose',
-            img: 'assets/images/green-rose.jpg',
-        }, {
-            name: 'green-rose',
-            img: 'assets/images/green-rose.jpg',
-        }, {
-            name: 'pink-rose',
-            img: 'assets/images/pink-rose.jpg',
-        }, {
-            name: 'pink-rose',
-            img: 'assets/images/pink-rose.jpg',
-        },
+//create an empty Array to store the opened cards
+let opened = [];
 
-        {
-            name: 'red-rose',
-            img: 'assets/images/red-rose.png',
-        }, {
-            name: 'red-rose',
-            img: 'assets/images/red-rose.png',
-        }, {
-            name: 'yellow-rose',
-            img: 'assets/images/yellow-rose.jpg',
-        }, {
-            name: 'yellow-rose',
-            img: 'assets/images/yellow-rose.jpg',
-        }
-    ]
- })
+// create an empty Array to store the matched cards
+let matched = [];
 
-let grid = document.querySelector('grid');
+// Access the model
+let modal = document.getElementById("model");
+// Access the reset button
+let reset = document.querySelector(".reset-btn");
+// Access the play again button
+let playAgain = document.querySelector(".play-again-btn");
 
- //create your board
- function createBoard() {
-     for (let i=0; i < cardArray.length; i++) {
-         let card = document.createElement('img')
-         card.setAttribute('src', 'assets/images/blank.png')
-         card.setAttribute('data-id', i)
-         //card.addEventListener('click', flipcard)
-         grid.appendChild(card)
-     }
- }
+//Select the class move counter and change its HTML
+let movesCount = document.querySelector(".moves-counter");
 
- //check for matches
+// Create variable for moves counter, start the count at zero
+let moves = 0;
+let star = document.getElementById("star-rating").querySelectorAll(".star");
 
+let starCount = 3;
+let timeCounter = document.querySelector(".timer");
 
- //flip your card
- function flipcard() {
-     let cardId
- }
+let time;
 
- createBoard
+let minutes = 0;
+
+let seconds = 0;
+
+let timeStart = false;
+
+function shuffle(array) {
+    let currentIndex = array.length,temporaryValue, randomIndex;
+    while (currentIndex !==0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+    return array;
+}

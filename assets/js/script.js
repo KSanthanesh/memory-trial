572ec66f617b788778635a521ee1ac0519af2057
KSanthanesh <santhanesh_2004@yahoo.com>
2021-06-12 11:11:20 +0000

    Add style for the code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-const cards = document.querySelectorAll('.memory-card');
+const cards = document.querySelectorAll('.rose-card');
 
 let hasFlippedCard = false;
 let lockBoard = false;
@@ -22,7 +22,7 @@ function flipCard() {
 }
 
 function checkForMatch() {
-  let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+  let isMatch = firstCard.dataset.base === secondCard.dataset.base;
 
   isMatch ? disableCards() : unflipCards();
 }
